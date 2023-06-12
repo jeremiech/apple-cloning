e591d850988381b11a54139554177fa40cc6ae5c
Jeje <jeremiech06@gmail.com>
2023-06-13 00:53:36 +0200

    add cloned index change

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './index.html'
+  ],
+  theme: {
+    screens:{
+        'xs':'390px',
+        'xl':	'1280px'
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
