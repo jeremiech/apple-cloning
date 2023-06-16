0659553493ee4085a796bc726517633dd1598800
Jeje <jeremiech06@gmail.com>
2023-06-16 23:43:10 +0200

    save me swiper change

---
+++ swipper-js/script.js
@@ -0,0 +1,16 @@
+const swiper = new Swiper('.swiper', {
+  loop: true,
+  cssMode: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  mousewheel: true,
+  keyboard: true,
+ 
+
+
+})
